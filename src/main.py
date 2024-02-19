26450abba20e3d566667de890a6d91107bbd6384
CanerEkinci37 <canereknc37@gmail.com>
2024-02-19 23:02:14 +0300

    chore: include routers to main file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
+from . import models
+from .routers import categories, businesses, index
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+app.include_router(index.router)
+app.include_router(categories.router)
+app.include_router(businesses.router)
