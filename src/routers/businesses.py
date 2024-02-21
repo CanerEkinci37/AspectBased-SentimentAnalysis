e59842b8c511f9bf3d6b9ea60b4efaf5fc3e859a
CanerEkinci37 <canereknc37@gmail.com>
2024-02-21 19:43:22 +0300

    feat: added get and compare business api

--- src/routers/businesses.py
+++ src/routers/businesses.py
@@ -68,6 +68,129 @@ def process_otel_form(
 
 
 # Businesses
+
+
+@router.get("/businesses/", tags=["businesses"])
+async def get_business(
+    business_category: str,
+    business_name: str,
+    db: Session = Depends(dependencies.get_db),
+):
+    business_category_id = crud.get_business_category_id(db=db, name=business_category)
+    if business_category_id:
+        business_id = crud.get_business_id(
+            db=db, name=business_name, business_category_id=business_category_id
+        )
+        if business_id:
+            if business_category == "restaurant":
+                _, category_sentiment_count = (
+                    helpers.get_restaurant_category_sentiments()
+                )
+            else:
+                _, category_sentiment_count = helpers.get_otel_category_sentiments()
+            for aspect, sentiments in category_sentiment_count.items():
+                aspect_category_id = crud.get_aspect_category_id(
+                    db=db, name=aspect, business_category_id=business_category_id
+                )
+                for sentiment in sentiments:
+                    sentiment_id = crud.get_sentiment_category_id(db=db, name=sentiment)
+                    aspect_sentiment_category_id = (
+                        crud.get_aspect_sentiment_category_id(
+                            db=db,
+                            sentiment_id=sentiment_id,
+                            aspect_category_id=aspect_category_id,
+                        )
+                    )
+                    business_record = crud.get_business_record(
+                        db=db,
+                        business_id=business_id,
+                        aspect_sentiment_category_id=aspect_sentiment_category_id,
+                    )
+                    category_sentiment_count[aspect][sentiment] = business_record.count
+            return {f"{business_name.title()}": category_sentiment_count}
+        raise HTTPException(
+            status_code=404,
+            detail=f"There is not such a {business_name.title()} business name",
+        )
+    raise HTTPException(
+        status_code=404,
+        detail=f"There is not like {business_category.title()} business category",
+    )
+
+
+@router.get("/compare_businesses/", tags=["businesses"])
+async def compare_businesses(
+    business_category: str,
+    business_name_first: str,
+    business_name_second: str,
+    db: Session = Depends(dependencies.get_db),
+):
+    business_category_id = crud.get_business_category_id(db=db, name=business_category)
+    if business_category_id:
+        business_first_id = crud.get_business_id(
+            db=db, name=business_name_first, business_category_id=business_category_id
+        )
+        business_second_id = crud.get_business_id(
+            db=db, name=business_name_second, business_category_id=business_category_id
+        )
+        if business_first_id is not None and business_second_id is not None:
+            if business_category == "restaurant":
+                _, category_sentiment_count_first = (
+                    helpers.get_restaurant_category_sentiments()
+                )
+                _, category_sentiment_count_second = (
+                    helpers.get_restaurant_category_sentiments()
+                )
+            else:
+                _, category_sentiment_count_first = (
+                    helpers.get_otel_category_sentiments()
+                )
+                _, category_sentiment_count_second = (
+                    helpers.get_otel_category_sentiments()
+                )
+            for aspect, sentiments in category_sentiment_count_first.items():
+                aspect_category_id = crud.get_aspect_category_id(
+                    db=db, name=aspect, business_category_id=business_category_id
+                )
+                for sentiment in sentiments:
+                    sentiment_id = crud.get_sentiment_category_id(db=db, name=sentiment)
+                    aspect_sentiment_category_id = (
+                        crud.get_aspect_sentiment_category_id(
+                            db=db,
+                            sentiment_id=sentiment_id,
+                            aspect_category_id=aspect_category_id,
+                        )
+                    )
+                    business_record_first = crud.get_business_record(
+                        db=db,
+                        business_id=business_first_id,
+                        aspect_sentiment_category_id=aspect_sentiment_category_id,
+                    )
+                    business_record_second = crud.get_business_record(
+                        db=db,
+                        business_id=business_second_id,
+                        aspect_sentiment_category_id=aspect_sentiment_category_id,
+                    )
+                    category_sentiment_count_first[aspect][
+                        sentiment
+                    ] = business_record_first.count
+                    category_sentiment_count_second[aspect][
+                        sentiment
+                    ] = business_record_second.count
+            return {
+                f"{business_name_first.title()}": category_sentiment_count_first,
+                f"{business_name_second.title()}": category_sentiment_count_second,
+            }
+        raise HTTPException(
+            status_code=404,
+            detail="One of the business names are wrong or in wrong business category",
+        )
+    raise HTTPException(
+        status_code=404,
+        detail=f"There is not like {business_category.title()} business category",
+    )
+
+
 @router.post("/create_business/", tags=["businesses"])
 async def create_business(
     business_category: str,
