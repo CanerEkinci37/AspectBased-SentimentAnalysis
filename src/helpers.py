2abebd7820d8d9ccfb99b2d756a99065f76eab32
CanerEkinci37 <canereknc37@gmail.com>
2024-02-25 12:32:59 +0300

    refactor: get_model and get_category_sentiments fuctions changed

--- src/helpers.py
+++ src/helpers.py
@@ -14,48 +14,22 @@ def handle_dataset(file: UploadFile = File(...)):
     return df
 
 
-# Restaurants
+# Businesses
+def get_model(category: str):
+    with open(f"./saved_models/{category}/ml_model.joblib", "rb") as f:
+        MODEL = joblib.load(f)
+    return MODEL
 
 
-def get_restaurant_model():
-    with open("./saved_models/restaurant/restaurant_literature1.joblib", "rb") as f:
-        RESTAURANT_MODEL = joblib.load(f)
-    return RESTAURANT_MODEL
-
-
-def get_restaurant_category_sentiments():
+def get_category_sentiments(category: str):
     category_sentiment_dict = {}
     category_count_dict = {}
-
-    aspects = ["PRICE", "FOOD", "SERVICE", "AMBIENCE", "MISCELLANEOUS"]
     sentiments = ["NEGATIVE", "NEUTRAL", "POSITIVE"]
 
-    count = 0
-    for aspect in aspects:
-        category_count_dict[aspect] = {}
-        for sentiment in sentiments:
-            category_sentiment_dict[count] = f"{aspect}#{sentiment}"
-            category_count_dict[aspect][sentiment] = 0
-            count += 1
-    return category_sentiment_dict, category_count_dict
-
-
-# Otels
-
-
-def get_otel_model():
-    with open("./saved_models/otel/otel_literature1.joblib", "rb") as f:
-        OTEL_MODEL = joblib.load(f)
-    return OTEL_MODEL
-
-
-def get_otel_category_sentiments():
-    category_sentiment_dict = {}
-    category_count_dict = {}
-
-    aspects = ["SERVICE", "MEAL", "LOCATION", "COMFORT", "CLEANLINESS"]
-    sentiments = ["NEGATIVE", "NEUTRAL", "POSITIVE"]
-
+    if category == "restaurant":
+        aspects = ["PRICE", "FOOD", "SERVICE", "AMBIENCE", "MISCELLANEOUS"]
+    elif category == "otel":
+        aspects = ["SERVICE", "MEAL", "LOCATION", "COMFORT", "CLEANLINESS"]
     count = 0
     for aspect in aspects:
         category_count_dict[aspect] = {}
